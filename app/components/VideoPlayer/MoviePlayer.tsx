07913c298e2c0dfd52b2d4932977e9b758ef21f8
Saba <khubashvili.saba12@gmail.com>
2023-05-29 19:47:21 +0400

    Version(0.7.5 Changed Movie Player)

--- app/components/VideoPlayer/MoviePlayer.tsx
+++ app/components/VideoPlayer/MoviePlayer.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import { BigPlayButton, Player } from 'video-react';
+import "node_modules/video-react/dist/video-react.css"; 
 
 
 export const MoviePlayer = () => {
@@ -7,12 +7,16 @@ export const MoviePlayer = () => {
 
   return (
     <div className='w-full h-full'>
-      <ReactPlayer  
-        controls
-        url={'https://f.vimeocdn.com/_videos/home/desktop-masthead-720p-2.mp4'}
-        width={'100%'}
-        height={'100%'}
-      />
+    <Player
+      playsInline
+      poster="/Image/movies/topgun.webp"
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      width={1000}
+      height={1000}
+
+    >
+      <BigPlayButton position="center" className='' />
+    </Player>
     </div>
   );
 };
