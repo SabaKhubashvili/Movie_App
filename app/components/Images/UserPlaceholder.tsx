b6e3f532766cfc8b02afe5a1268d54597e78e315
Saba Khubashvili <khubashvili.saba12@gmail.com>
2023-10-31 23:39:10 +0400

    fix: fixed user placeholder iamge

--- app/components/Images/UserPlaceholder.tsx
+++ app/components/Images/UserPlaceholder.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const UserPlaceholder = () => {
   return (
     <Image
-        src={'/Image/user/placeholder.webp'}
+        src={'/Image/user/Placeholder.webp'}
         alt='userPlaceholder'
         width={100}
         height={100}
